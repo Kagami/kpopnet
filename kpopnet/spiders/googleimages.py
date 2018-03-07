0696f7a6130b81e17c665bb076a7161a042cfe58
Kagami Hiiragi <kagami@genshiken.org>
2018-03-07 16:37:52 +0300

    Parallelize images download

--- kpopnet/spiders/googleimages.py
+++ kpopnet/spiders/googleimages.py
@@ -17,14 +17,14 @@ class GoogleimagesSpider(ImageSpider):
 
     def build_search_url(self, bname, mname):
         return 'https://www.google.com/search?' + urlencode([
-            ('q', '{} {}'.format(bname, mname)),
+            ('q', '{} {}'.format(bname, mname)),  # Search query
             ('espv', '2'),
             ('biw', '1366'),
             ('bih', '667'),
             ('site', 'webhp'),
             ('source', 'lnms'),
             ('tbm', 'isch'),
-            ('tbs', 'itp:face,ift:jpg'),
+            ('tbs', 'itp:face,ift:jpg'),  # Face type, JPEG format
             ('sa', 'X'),
             ('ei', 'XosDVaCXD8TasATItgE'),
             ('ved', '0CAcQ_AUoAg'),
@@ -41,8 +41,15 @@ class GoogleimagesSpider(ImageSpider):
     def parse(self, response):
         response.meta['_knet_num_saved'] = 0
         response.meta['_knet_items'] = response.css(".rg_meta")
-        # TODO(Kagami): Speedup download by emitting MAX_N requests at
-        # the same time and fetching remaining after that?
+
+        # Do first N requests in parallel.
+        response.meta['_knet_parallel'] = True
+        for _ in range(self.MAX_IMAGES_PER_MEMBER):
+            yield from self.parse_item(response.meta)
+
+        # Fetch remaining.
+        # TODO(Kagami): We can parallel that too.
+        response.meta['_knet_parallel'] = False
         return self.parse_item(response.meta)
 
     def parse_item(self, meta):
@@ -51,9 +58,10 @@ class GoogleimagesSpider(ImageSpider):
         try:
             item = meta['_knet_items'].pop(0)
         except IndexError:
-            self.logger.warning('Saved only {} images for {} - {}'.format(
-                meta['_knet_num_saved'], meta['_knet_bname'],
-                meta['_knet_mname']))
+            if not meta['_knet_parallel']:
+                self.logger.warning('Saved only {} images for {} - {}'.format(
+                    meta['_knet_num_saved'], meta['_knet_bname'],
+                    meta['_knet_mname']))
             return
         try:
             item_text = item.css("::text").extract_first()
@@ -70,7 +78,9 @@ class GoogleimagesSpider(ImageSpider):
     def save_item(self, response):
         if self.save_image(response):
             response.meta['_knet_num_saved'] += 1
-        return self.parse_item(response.meta)
+        if not response.meta['_knet_parallel']:
+            return self.parse_item(response.meta)
 
     def errback_item(self, failure):
-        return self.parse_item(failure.request.meta)
+        if not failure.request.meta['_knet_parallel']:
+            return self.parse_item(failure.request.meta)
