32fb5ec330030eeb31c8920ad9bb27356117b406
Kagami Hiiragi <kagami@genshiken.org>
2018-04-03 19:19:55 +0300

    Imported images are confirmed

--- go/src/kpopnet/sql/upsert_face.sql
+++ go/src/kpopnet/sql/upsert_face.sql
@@ -1,2 +1,2 @@
-INSERT INTO faces (descriptor, image_id, idol_id) VALUES ($1, $2, $3)
+INSERT INTO faces (descriptor, image_id, idol_id, idol_confirmed) VALUES ($1, $2, $3, TRUE)
 ON CONFLICT (image_id, idol_id) DO NOTHING
